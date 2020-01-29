341038814de51ff3b32da907ff9776530f498a7e
Nicholas Zatkovich <nzatkovich@gmail.com>
2020-01-29 14:32:25 -0800

    burn all fonts

--- runtrace.sh
+++ runtrace.sh
@@ -3,6 +3,5 @@
 # This script runs strace for debugging
 # use '-e' option to select which calls you want or don't want.
 
-#sudo strace -u $USER -e 'open,openat,open_by_handle_at,execve' -f -D -vv -o ./subedit.trace.log  /snap/bin/subtitle-edit $@
-
-sudo strace -u $USER -e '!select,pselect6,_newselect,clock_gettime,sigaltstack,gettid,gettimeofday,nanosleep' -f -D -vv -o ./subedit.trace.log  /snap/bin/subtitle-edit $@
+sudo strace -u $USER -e 'open,openat,open_by_handle_at,execve' -f -D -vv -o ./subedit.trace.log  /snap/bin/subtitle-edit $@
+#sudo strace -u $USER -e '!select,pselect6,_newselect,clock_gettime,sigaltstack,gettid,gettimeofday,nanosleep' -f -D -vv -o ./subedit.trace.log  /snap/bin/subtitle-edit $@
