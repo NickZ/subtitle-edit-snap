3cdb1bf1d479e7b26664ea49ad097ed0566df48a
Nicholas Zatkovich <nzatkovich@gmail.com>
2020-01-30 17:26:26 -0800

    Fix bug with fonts not being displayed properly, update to 3.5.13-3

--- runtrace-bare.sh
+++ runtrace-bare.sh
@@ -3,5 +3,5 @@
 # This script runs strace for debugging
 # use '-e' option to select which calls you want or don't want.
 
-sudo strace -u $USER -e 'open,openat,open_by_handle_at,execve,stat,access,fstat,newfstatat' -f -s 255 -D -vv -o ./subeditbare.trace.log  /snap/bin/subtitle-edit $@
+sudo strace -u $USER -e 'open,openat,open_by_handle_at,execve,stat,access,fstat,newfstatat' -f -s 255 -D -vv -o ./baresubedit.trace.log mono ./subtitleedit/SubtitleEdit.exe
 #sudo strace -u $USER -e '!select,pselect6,_newselect,clock_gettime,sigaltstack,gettid,gettimeofday,nanosleep' -f -D -vv -o ./subedit.trace.log  /snap/bin/subtitle-edit $@
